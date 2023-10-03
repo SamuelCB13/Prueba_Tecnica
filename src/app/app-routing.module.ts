54608bc23567208a114edb97c5fe093008e98aff
Samuel Carvajal <casato311409@gmail.com>
2023-10-03 10:45:37 -0500

    Segundo commit - Prueba técnica

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -4,12 +4,14 @@ import { FilmsComponent } from './components/films/films.component';
 import { HomeComponent } from './components/home/home.component';
 import { SeriesComponent } from './components/series/series.component';
 import { NavbarComponent } from './components/navbar/navbar.component';
+import { FooterComponent } from './components/footer/footer.component';
 
 const routes: Routes = [
   { path: '', component: HomeComponent },
   { path: 'films', component: FilmsComponent },
   { path: 'series', component: SeriesComponent },
   { path: 'navbar', component: NavbarComponent },
+  { path: 'footer', component: FooterComponent },
 ];
 
 @NgModule({
